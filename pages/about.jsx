74281698be98d8b615be7458a0be7cb871cf5d1a
jjia1 <jdjia93@gmail.com>
2024-06-28 20:33:22 -0500

    fixed resume and ml images

--- pages/about.jsx
+++ pages/about.jsx
@@ -23,26 +23,30 @@ const AboutPage = () => {
           <span role="img" aria-label="book">📖</span> Senior Data Analyst @ <a href='http://liuzlab.org/members/'>Baylor College of Medicine</a>.
         </li>
         <li>
-          <span role="img" aria-label="graduate-hat">🎓</span> Data analytics for <b>Deep-learning</b>, <b>Bioinformatics</b>, <b>Multiomics</b> , and <b>Biomedical Informatics</b> @ <a href='https://github.com/zhandong'>Zhandong Liu Lab Lab</a>.
+          <span role="img" aria-label="graduate-hat">🎓</span> Analytics <b>LLMs</b>, <b>NLP</b>, <b>Machine Learning</b>, <b>Bioinformatics</b>, <b>Multiomics</b>, and <b>Biomedical Informatics</b> @ <a href='https://github.com/zhandong'>Zhandong Liu Lab</a>.
         </li>
         <li>
           <span role="img" aria-label="laptop">💻</span> Currently developing new high throughput methods to leverage patient phenotypic, genotypic, and multiomic data for rapid, precision clinical diagnostics
         </li>
       </ul>
       <br />
-      <center>
-        <h3>Resume (<a href={myResume} download="JohnathanJia-Resume.pdf">Download</a>)</h3>
+      <div style={{ width: '100%', maxWidth: '800px', margin: '0 auto', padding: '1rem', backgroundColor: 'transparent', textAlign: 'center' }}>
+      <h3>Resume (<a href={myResume} download="JohnathanJia-Resume.pdf">Download</a>)</h3>
         <br />
         <Document
           file={myResume}
           onLoadSuccess={onDocumentLoadSuccess}
           onLoadError={(error) => console.error('Failed to load PDF:', error)}
         >
-          {Array.from(new Array(numPages), (el, index) => (
-            <Page key={`page_${index + 1}`} pageNumber={index + 1} renderMode="svg" />
-          ))}
+          {numPages && 
+            Array.from(new Array(numPages), (el, index) => (
+              <Page key={`page_${index + 1}`} pageNumber={index + 1} renderMode="canvas" 
+              scale={3} // Increase scale for higher quality
+              />
+            ))
+          }
         </Document>
-      </center>
+      </div>
     </>
   );
 };
